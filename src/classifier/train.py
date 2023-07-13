fe9da2269572edffbfd75e889c9c83802f1cbcce
DCisHurt <roger33669@gmail.com>
2023-07-13 17:39:51 +0100

    add tensorboard

--- src/classifier/train.py
+++ src/classifier/train.py
@@ -1,6 +1,9 @@
 import torch
 from torch.utils.data import DataLoader
 from torch import nn
+import torch.optim.lr_scheduler as lr_scheduler
+
+EFFECT_MAP = ["distortion", "chorus", "tremolo", "delay", "reverb"]
 
 
 def create_data_loader(train_data, batch_size):
@@ -11,10 +14,22 @@ def create_data_loader(train_data, batch_size):
 def train_single_epoch(model, data_loader, loss_fn, optimiser, device, mode="multi", effect=0):
 
     model.train()
-    size = len(data_loader)
+    sigmoid = nn.Sigmoid()
+
+    batch_size = data_loader.batch_size
+    total_size = len(data_loader.dataset)
+    nBatch = len(data_loader)
+
+    if mode == "single":
+        total_loss, correct = 0, 0
+        log = []
+    else:
+        total_loss, correct = [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]
+        log = [[], [], [], [], []]
+
     for batch, data in enumerate(data_loader):
 
-        X, labels, _, _, _ = data
+        X, labels, _, _, filenames = data
         X = X.to(device)
 
         optimiser.zero_grad()
@@ -25,21 +40,44 @@ def train_single_epoch(model, data_loader, loss_fn, optimiser, device, mode="mul
 
         if mode == "single":
             loss = loss_fn(preds, labels[effect])
+            total_loss += loss
+            for index, name in enumerate(filenames):
+                result = 1 if sigmoid(preds[index]) > 0.5 else 0
+                expected = 1 if labels[effect][index].item() == 1.0 else 0
+                log.append([name, result, expected])
+                correct += 1.0 if result == expected else 0.0
         else:
             loss = 0
+            i = 0
             for output, target in zip(preds, labels):
                 loss += loss_fn(output, target)
+                total_loss[i] += loss
+                for index, name in enumerate(filenames):
+                    result = 1 if sigmoid(output[index]) > 0.5 else 0
+                    expected = 1 if target[index].item() == 1.0 else 0
+                    log[i].append([name, result, expected])
+                    correct[i] += 1.0 if result == expected else 0.0
+                i += 1
 
         loss.backward()
         optimiser.step()
 
         if batch % 20 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f'loss: {loss:>8f}  [{current:>3d}/{size * len(X)}]')
+            loss, current = loss.item(), batch * batch_size
+            print(f'loss: {loss:>8f}  [{current:>3d}/{total_size}]')
+
+    if mode == "single":
+        total_loss /= nBatch
+        correct /= total_size
+    else:
+        for i in range(len(loss)):
+            total_loss[i] /= nBatch
+            correct[i] /= total_size
+
+    return total_loss, correct, log
 
 
 def test(model, data_loader, device, loss_fn=None, mode="multi", effect=0):
-    EFFECT_MAP = ["distortion", "chorus", "tremolo", "delay", "reverb"]
 
     size = len(data_loader.dataset)
     nBatch = len(data_loader)
@@ -88,7 +126,7 @@ def test(model, data_loader, device, loss_fn=None, mode="multi", effect=0):
     if mode == "single":
         loss /= nBatch
         correct /= size
-        print(f'Accuracy: {(100*correct):>0.1f}%, avg loss: {loss:>8f}\n')
+        print(f'Accuracy: {(100*correct):>0.1f}%, avg loss: {loss:>8f}')
     else:
         total_loss = 0
         for i in range(len(loss)):
@@ -97,14 +135,14 @@ def test(model, data_loader, device, loss_fn=None, mode="multi", effect=0):
             total_loss += loss[i]
             print(f'{EFFECT_MAP[i]}: Accuracy: {(100*correct[i]):>0.1f}%, avg loss: {loss[i]:>8f}')
         print(f'Total: avg loss: {total_loss:>8f}\n')
-    return log
+
+    return loss, correct, log
 
 
 def test_single(model, input_data, mode="multi", effect=0):
     model.eval()
     sigmoid = nn.Sigmoid()
-    # LABLE_MAP = ['bypass', 'activate']
-    # EFFECT_MAP = ["distortion", "chorus", "tremolo", "delay", "reverb"]
+
     result = []
     with torch.no_grad():
         X, labels, _, _, filename = input_data
@@ -130,10 +168,35 @@ def test_single(model, input_data, mode="multi", effect=0):
 
 
 def train(model, train_data_loader, test_data_loader, loss_fn, optimiser,
-          device, epochs, mode="multi", effect=0):
-    for i in range(epochs):
-        print(f"Epoch {i+1}")
-        train_single_epoch(model, train_data_loader, loss_fn, optimiser, device, mode, effect)
-        test(model, test_data_loader, device, loss_fn, mode, effect)
-        print("---------------------------")
+          device, writer, epochs, mode="multi", effect=0):
+    scheduler = lr_scheduler.LinearLR(optimiser, start_factor=1.0, end_factor=0.1, total_iters=10)
+    for epoch in range(1, epochs+1):
+        print(f"Epoch {epoch}")
+
+        tr_loss, tr_acc, _ = train_single_epoch(model,
+                                                train_data_loader,
+                                                loss_fn,
+                                                optimiser,
+                                                device,
+                                                mode,
+                                                effect)
+        ts_loss, ts_acc, _ = test(model, test_data_loader, device, loss_fn, mode, effect)
+
+        if mode == "single":
+            writer.add_scalars("Loss", {'train': tr_loss, 'test': ts_loss}, epoch)
+            writer.add_scalars("Accuracy", {'train': tr_acc, 'test': ts_acc}, epoch)
+        else:
+            for i in range(len(tr_loss)):
+                writer.add_scalars("Loss/" + EFFECT_MAP[i],
+                                   {'train': tr_loss[i], 'test': ts_loss[i]}, epoch)
+                writer.add_scalars("Accuracy/" + EFFECT_MAP[i],
+                                   {'train': tr_acc[i], 'test': ts_acc[i]}, epoch)
+
+        before_lr = optimiser.param_groups[0]["lr"]
+        writer.add_scalar("Learning Rate", before_lr, epoch)
+        scheduler.step()
+        after_lr = optimiser.param_groups[0]["lr"]
+
+        print("learning rate: %.6f -> %.6f" % (before_lr, after_lr))
+        print("---------------------------\n")
     print("Finished training")
